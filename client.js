542821375ead9379f82b58b5fd11eb19974c6df6
psoto <psoto23.ps@gmail.com>
2014-08-18 18:37:48 -0300

    Fix relation path id and path value

--- client.js
+++ client.js
@@ -2,22 +2,27 @@ ES = ES || {};
 
 ES.syncCollection = function(options) {
   var collection = options.collection;
+
   collection.esSearch = function(searchString, cb) {
-  	var query = {
+  	var splitedSearchString = searchString.split(" ");
+    var query = {
   		bool: {
         should: [],
       }
   	};
     var q = query.bool.should;
+
     q.regexp = {};
 	  _.forEach(options.fields, function(field) {
       if (_.isObject(field)) {
         field = field.name;
       }
-      
-      var regexp = {};
-      regexp[field] = searchString;
-      q.push({regexp: regexp});
+
+      _.forEach(splitedSearchString, function(tokenSearch) {
+        var regexp = {};
+        regexp[field] = tokenSearch;
+        q.push({regexp: regexp});
+      })
 	  });
 
     console.log(query)
@@ -26,17 +31,7 @@ ES.syncCollection = function(options) {
   			console.log(result);
   		}
 
-  		cb && cb.call(err, result);
+  		cb && cb.call({}, err, result);
   	});
-  };
-
-  collection.esSuggest = function(text, cb) {
-    Meteor.call('esSuggest', collection._name, text, function(err, result) {
-      if (!err) {
-        console.log(result);
-      }
-
-      cb && cb.call(err, result);
-    });
   };
 }
